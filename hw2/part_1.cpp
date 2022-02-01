678b83c77a517adf0924c92c5d4cf65e7b513a2b
Ishai Masada <imasada@vtcap.org>
2022-02-01 16:08:34 -0500

    made some kinda changes. maybe

---
+++ hw2/part_1.cpp
@@ -0,0 +1,11 @@
+/* Ishai Masada
+ * Salary */
+
+#include <iostream>
+
+int main()
+{
+    double percent_increase = 7.6;
+    int time = 6; // Months
+    int prev_salary = cin >> "Type in your previous salary: " >> endl;
+}
