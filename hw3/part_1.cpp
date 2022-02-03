6a1e1bd5713ff562f96e90ef2e8a4e05925ae839
Ishai Masada <imasada@vtcap.org>
2022-02-03 18:22:32 -0500

    boiler plate and two numbers

---
+++ hw3/part_1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+double main()
+{
+    double first_num = cin >> "Type in the first number: " >> endl;
+    double second_num = cin >> "Type in the second number: " >> endl;
+}
