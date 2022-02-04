78359658689e115e217a205ae9a1a2b9a0ec54b9
Ishai Masada <imasada@vtcap.org>
2022-02-03 19:07:06 -0500

    created all of the variables and added print statements

--- hw3/part_1.cpp
+++ hw3/part_1.cpp
@@ -2,8 +2,29 @@
 
 using namespace std;
 
-double main()
+int main()
 {
-    double first_num = cin >> "Type in the first number: " >> endl;
-    double second_num = cin >> "Type in the second number: " >> endl;
+    int first_num = cin >> "Type in the first number: " >> endl;
+    int second_num = cin >> "Type in the second number: " >> endl;
+
+    if (second_num != 0)
+    {
+        int sum = first_num + second_num;
+        cout << "Sum of the two numbers: " << sum << endl;
+    }
+    else
+    {
+        cout << "You cannot divide any number by 0." << endl;
+    }
+
+    int difference = first_num - second_num;
+    cout << "Difference of the two numbers: " << difference << endl;
+
+    int quotient = first_num / second_num;
+    cout << "Quotient of the two numbers: " << quotient << endl;
+
+    int product = first_num * second_nim;
+    cout << "Product of the two numbers: " << product << endl;
+
+    return 0;
 }
