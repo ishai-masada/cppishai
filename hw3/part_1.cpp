b8381afe826266c822b8836aa7a0b3d5ee346f92
Ishai Masada <imasada@vtcap.org>
2022-02-03 20:05:14 -0500

    added print statements

--- hw3/part_1.cpp
+++ hw3/part_1.cpp
@@ -4,27 +4,35 @@ using namespace std;
 
 int main()
 {
-    int first_num = cin >> "Type in the first number: " >> endl;
-    int second_num = cin >> "Type in the second number: " >> endl;
+    int first_num;
+    cout << "Type in the first number: ";
+    cin >> first_num;
+
+    int second_num;
+    cout << "Type in the second number: ";
+    cin >> second_num;
+
+    cout << endl << "Each operation is always done using the first number before the second"
+         << endl;
 
     if (second_num != 0)
     {
-        int sum = first_num + second_num;
-        cout << "Sum of the two numbers: " << sum << endl;
+        int quotient = first_num / second_num;
+        cout << endl << "Quotient of the two numbers: " << quotient << endl;
     }
     else
     {
-        cout << "You cannot divide any number by 0." << endl;
+        cout << endl << "You cannot divide any number by 0." << endl;
     }
 
     int difference = first_num - second_num;
-    cout << "Difference of the two numbers: " << difference << endl;
+    cout << endl << "Difference of the two numbers: " << difference << endl;
 
-    int quotient = first_num / second_num;
-    cout << "Quotient of the two numbers: " << quotient << endl;
+    int sum = first_num + second_num;
+    cout << endl << "Sum of the two numbers: " << sum << endl;
 
-    int product = first_num * second_nim;
-    cout << "Product of the two numbers: " << product << endl;
+    int product = first_num * second_num;
+    cout  << endl << "Product of the two numbers: " << product << endl;
 
     return 0;
 }
