9846eaa598cb0aaf3c30eed49e56844fac7fd711
Ishai Masada <imasada@vtcap.org>
2022-02-06 17:57:12 -0500

    added comments and return 0 for every if statement

--- hw3/part_2.cpp
+++ hw3/part_2.cpp
@@ -1,47 +1,64 @@
 #include <iostream>
-#include <string>
 
 using namespace std;
 
-string main()
+int main()
 {
-    string super_premium = "Super Premium";
-    string premium = "Premium";
-    string normal = "Normal Quality";
-    string low_quality = "Not so great";
+    char super_premium[50] = "\"Super Premium\"";
+    char premium[50] = "\"Premium\"";
+    char normal[50] = "\"Normal Quality\"";
+    char low_quality[50] = "\"Not so great\"";
 
     // Get the percent of fat in the user's dog food
     int percent_fat;
-    cout << "Type in the percent of fat in your dog food: " >> endl;
-    cin >> percent_fat >> endl;
+    cout << "Type in the percent of fat in your dog food: ";
+    cin >> percent_fat;
 
     // Get the percent of protein in the user's dog food
     int percent_protein;
-    cout << "Type in the percent of protein in your dog food: " >> endl;
-    cin >> percent_protein >> endl;
+    cout << "Type in the percent of protein in your dog food: ";
+    cin >> percent_protein;
 
+    /* Check if the fat percentage is less than 18 and the protein percentage
+     * is greater than 23*/
     if (percent_fat < 18 and percent_protein > 23)
     {
-        cout << "Your dog food is a" << super_preminum << " quality dog food"
-             << endl;
+        printf("Your dog food is a %s quality dog food", super_premium);
+        cout << endl;
+        return 0;
     }
 
+    /* Check if the fat percentage is less than 20 and the protein percentage
+     * is greater than 20*/
     if (percent_fat < 20 and percent_protein > 20)
     {
-        cout << "Your dog food is a" << preminum << " quality dog food"
-             << endl;
+        printf("Your dog food is a %s quality dog food", premium);
+        cout << endl;
+        return 0;
     }
 
+    /* Check if the fat percentage is less than 25 and the protein percentage
+     * is greater than 16*/
     if (percent_fat < 25 and percent_protein > 16)
     {
-        cout << "Your dog food is a" << normal << " quality dog food"
-             << endl;
+        printf("Your dog food is a %s quality dog food", normal);
+        cout << endl;
+        return 0;
     }
 
+    /* Check if the fat percentage is less than 40 and the protein percentage
+     * is greater than 8*/
     if (percent_fat < 40 and percent_protein > 8)
     {
-        cout << "Your dog food is " << low_quality << " quality dog food"
-             << endl;
+        printf("Your dog food is a %s quality dog food", low_quality);
+        cout << endl;
+        return 0;
     }
+
+    else
+    {
+        cout << "Your dog food is unclassified" << endl;
+    }
+
     return 0;
 }
