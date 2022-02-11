d1e2e90b69e560b2e2b02fe70523b836b6e93d9b
Ishai Masada <imasada@vtcap.org>
2022-02-11 15:16:23 -0500

    Added comments.

--- hw4/part_1.cpp
+++ hw4/part_1.cpp
@@ -1,15 +1,20 @@
+/*Ishai Masada
+ * Print out the numbers from 19 to 65 except for 46 */
+
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
+    // Counts from 19 to 65. Excludes 46
     for (int i=19; i < 66; i++)
     {
         if (i == 46)
         {
             continue;
         }
+        // Prints the number
         printf("%d\n", i);
     }
 }
