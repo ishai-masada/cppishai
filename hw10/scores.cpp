0bdf78a29e3e7d15163f8a63098a56aa755af9c5
Ishai Masada <imasada@vtcap.org>
2022-04-04 09:07:01 -0400

    finished the program with comments

--- hw10/scores.cpp
+++ hw10/scores.cpp
@@ -2,14 +2,19 @@
 #include <fstream>
 #include <cstring>
 #include <string.h>
+#include <sstream>
+#include <vector>
 
 using namespace std;
 
-bool is_number(string value)
+// Checks if string is a number
+bool is_number(string word)
 {
-    for (int i = 0; i < value.length(); i++)
+    // Loop over word
+    for (int i = 0; i < word.size(); i++)
     {
-        if (isdigit(value[i]) == false)
+        // Checks if each character is a number
+        if (isdigit(word[i]) == false)
         {
             return false;
         }
@@ -18,14 +23,68 @@ bool is_number(string value)
     return true;
 }
 
-int main()
+// Returns the average given the sum and the numebr of values
+double avg(int sum, int num_of_values)
 {
-    int ARRAY_SIZE = 12;
-    string scores[ARRAY_SIZE];
-    string input_data[ARRAY_SIZE];
+    return sum / num_of_values;
+}
 
+// Calculates the average scores of each student
+vector<int> get_averages(vector<int> averages, vector<string> sentences)
+{
+    int NUM_OF_SCORES = 10;
+
+    // Template word
+    string word;
+
+    int sum = 0;
+    int counter = 0;
+
+    // Iterates over each sentence in the input data vector
+    for (int i = 0; i < sentences.size(); i++)
+    {
+        // Creates a string stream object out of each sentence
+        istringstream string_obj(sentences[i]);
+
+        // Keeps going until there aren't any words left
+        while (string_obj >> word)
+        {
+            // Checks if the word is a number
+            if (is_number(word))
+            {
+                counter += 1;
+                sum += stoi(word);
+
+                // Checks if this is the last score for the student
+                if (counter >= NUM_OF_SCORES)
+                {
+                    averages.push_back(avg(sum, NUM_OF_SCORES));
+                    sum = 0;
+                    counter = 0;
+                }
+            }
+        }
+    }
+
+    return averages;
+}
+
+// Displays the average scores for each student
+void display(vector<int> averages)
+{
+    // Loops over the averages vector
+    for (int i = 0; i < averages.size(); i++)
+    {
+        cout << "Average: " << averages[i] << endl;
+    }
+}
+
+// Handles file streams and displays the average scores
+void calculate_avg_scores(vector<int> averages)
+{
     ifstream istream;
     ofstream ostream;
+    vector<string> input_data;
 
     // Initialize the Input stream with the data text file
     istream.open("StudentScores.txt");
@@ -37,13 +96,11 @@ int main()
         exit(1);
     }
 
-    int sum = 0;
-    int count = 0;
-    double average = 0;
-
-    for (int i = 0; i < ARRAY_SIZE; i++)
+    while (istream)
     {
-        getline(istream, input_data[i], ' ');
+        string line;
+        getline(istream, line);
+        input_data.push_back(line);
         if (istream.eof())
         {
             istream.close();
@@ -51,16 +108,67 @@ int main()
         }
     }
 
-    for (int i = 0; i < ARRAY_SIZE; i++)
+    // Initialize the Input stream with the data text file
+    ostream.open("StudentScores.txt");
+
+    // Check if the file fails to open
+    if (ostream.fail())
     {
-        cout << "Value: " << input_data[i] << endl << "Bool: "
-             << is_number(input_data[i]) << endl;
+        cout << "File openning failed\n";
+        exit(1);
     }
 
-    /* sum += stoi(input_data[i]); */
-    /* count += 1; */
-    /* average = sum / count; */
-    /* cout << "Average: " << average << endl; */
+    averages = get_averages(averages, input_data);
+    display(averages);
+
+    for (int i = 0; i < averages.size(); i++)
+    {
+        ostream << input_data[i]<< " " << averages[i] << endl;
+    }
+
+    ostream.close();
+}
+
+int main()
+{
+    vector<string> scores;
+    vector<string> words;
+    vector<int> averages;
+    string choice;
+
+
+    // Prompt the user for their choice
+    cout << "Would you like to calculate the averages of the students' scores?"
+         << "(\"Yes\" or \"No\"): "<< endl;
+    cin >> choice;
+
+    while (true)
+    {
+        // Checks if the user wants to end the program
+        if (choice == "No")
+        {
+            cout << "Ending program." << endl;
+            return 0;
+        }
+
+        // Checks if the user wants to continue
+        else if (choice == "Yes")
+        {
+            break;
+        }
+
+        else
+        {
+            cout << "You did not enter a valid response." << endl;
+        }
+
+        // Prompt the user
+        cout << "Would you like to calculate the averages of the students' scores?"
+             << "(\"Yes\" or \"No\"): "<< endl;
+        cin >> choice;
+    }
+
+    calculate_avg_scores(averages);
 
    return 0;
 }
