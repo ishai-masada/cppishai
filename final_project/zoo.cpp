fce3778e26a02c241c0cb9a50cb15588b929db40
Ishai Masada <imasada@vtcap.org>
2022-04-15 16:16:50 -0400

    something happened

--- final_project/zoo.cpp
+++ final_project/zoo.cpp
@@ -5,21 +5,41 @@
 
 using namespace std;
 
-const int TICKET_COST = 60;
+const int TICKET_COST = 50;
 
-struct animal
+class Animal
 {
+public:
     string name;
+
     int cost = 0;
 
-    // Customers per day
-    int earning_rate = 0;
+    // Number of this animal
+    int count = 0;
+
+    // Constuctor for the Animal class
+    Animal(string name, int cost)
+    {
+        this->name = name;
+        this->cost = cost;
+    }
+
+    float get_earning_rate()
+    {
+        // Customers per day
+        float earning_rate = cost / TICKET_COST;
+
+        return earning_rate;
+    }
 };
 
+// Checks if a string is a number
 bool is_number(string num)
 {
+    // Iterates over the string
     for (int i; i < num.size(); i++)
     {
+        // Checks if the character is a number
         if (isdigit(num[i]) == false)
         {
             return false;
@@ -29,14 +49,17 @@ bool is_number(string num)
     return true;
 }
 
-bool make_purchase(string animal_name, int animal_cost, int& budget)
+// Checks if the user can afford to purchase the desired animal
+bool make_purchase(Animal desired_animal, int budget)
 {
-    cout << endl << "animal cost: " << animal_cost << endl;
-    if (budget - animal_cost >= 0)
+    cout << endl << "animal cost: " << desired_animal.cost << endl;
+
+    // Checks if the user has enough funds to purchase the desired animal
+    if (budget - desired_animal.cost >= 0)
     {
-        budget -= animal_cost;
+        budget -= desired_animal.cost;
         cout << "budget: " << budget << endl;
-        cout << "You purchased a " << animal_name << "!" << endl;
+        cout << "You purchased a " << desired_animal.name << "!" << endl;
 
         return true;
     }
@@ -49,55 +72,10 @@ bool make_purchase(string animal_name, int animal_cost, int& budget)
     }
 }
 
-// TODO Fix the map -> vector/struct change
-vector<animal> purchase_animals(vector<animal> available_animals,
-                                  vector<animal> owned_animals,
-                                  string& animal_name, int& animal_cost,
-                                  int& budget)
+// Displays all animals of class Animal inside of a vector
+void display_animals(vector<Animal> animals)
 {
-    while (true)
-    {
-        cout << endl << "Type in the name of the animal you want to buy or nothing to end the program: ";
-        getline(cin, animal_name);
-
-        // Check if the user hit enter
-        if (animal_name.length() == 0)
-        {
-            return owned_animals;
-        }
-
-        else
-        {
-            // Check if the user entered a number
-            if (is_number(animal_name))
-            {
-                cout << endl << "You can only enter letters" << endl;
-                continue;
-            }
-        }
-
-        for (int i = 0; i < available_animals.size(); i++)
-        {
-            if (animal_name == available_animals[i].name)
-            {
-                animal_cost = available_animals[i].cost;
-            }
-        }
-
-        // Check if the user can afford the animal they want to purchase
-        if (make_purchase(animal_name, animal_cost, budget))
-        {
-            // Store the animal into the user's owned animals
-            auto iterator = available_animals.find(animal_name);
-            owned_animals.insert({iterator->first, iterator->second});
-        }
-    }
-
-    return owned_animals;
-}
-
-void display_animals(vector<animal> animals)
-{
+    // Checks if the vector is empty
     if (animals.size() == 0)
     {
         cout << "(None)" << endl;
@@ -105,58 +83,126 @@ void display_animals(vector<animal> animals)
 
     else
     {
-        for (auto iterator = animals.begin(); iterator != animals.end(); iterator++)
+        // Iterates over each animal
+        for (int i = 0; i < animals.size(); i++)
         {
-            cout << iterator->first << " - $" << iterator->second << endl;
+            // Displays the name and cost of each animal
+            cout << animals[i].name << " - $" << animals[i].cost << endl;
         }
     }
 }
 
-int main()
+class Zoo
 {
-    // Available animals to buy
-    /* map<string, int> available_animals = { */
-    /*     {"Tiger", 7500}, */
-    /*     {"Lion", 15000}, */
-    /*     {"Bear", 15000}, */
-    /*     {"Monkey", 3500}, */
-    /*     {"Giraffe", 60000}, */
-    /*     {"Zebra", 4000}, */
-    /*     {"Rhino", 27000}, */
-    /*     {"Crocodile", 1100} */
-    /* }; */
+public:
+    string name;
 
-    // Available animals to buy
-    vector<animal> available_animals = {
-        {"Tiger", 7500, 50},
-        {"Lion", 15000, 50},
-        {"Bear", 15000, 50},
-        {"Monkey", 3500, 50},
-        {"Giraffe", 60000, 50},
-        {"Zebra", 4000, 50},
-        {"Rhino", 27000, 50},
-        {"Crocodile", 1100, 50}
+    // Vector to store all of the animals that the user owns
+    vector<Animal> owned_animals;
+
+    // Available animals to purchase
+    vector<Animal> available_animals = {
+        Animal("Tiger", 7500),
+        Animal("Lion", 15000),
+        Animal("Bear", 15000),
+        Animal("Monkey", 3500),
+        Animal("Giraffe", 60000),
+        Animal("Zebra", 4000),
+        Animal("Rhino", 27000),
+        Animal("Crocodile", 1100)
     };
 
-    /* // User's animals */
-    /* map<string, int> owned_animals; */
+    // Constructor for the Zoo class
+    Zoo(string name, vector<Animal> owned_animals)
+    {
+        this->name = name;
+        this->owned_animals = owned_animals;
+    }
 
+    // Allows the user to purchase animals for their zoo
+    void purchase_animals(int budget)
+    {
+        string animal_name;
+
+        // Template variable of Animal class type
+        Animal desired_animal("Name", 0);
+
+        while (true)
+        {
+            cout << endl << "Type in the name of the animal you want to buy or nothing to end the program: ";
+
+            // Stores the input from the user inside of the animal_name variable
+            getline(cin, animal_name);
+
+            // Check if the user hit enter
+            if (animal_name.length() == 0)
+            {
+                return;
+            }
+
+            else
+            {
+                // Check if the user entered a number
+                if (is_number(animal_name))
+                {
+                    cout << endl << "You can only enter letters" << endl;
+                    continue;
+                }
+            }
+
+            // Checks if the user entered an available animal
+            for (int i = 0; i < available_animals.size(); i++)
+            {
+                if (animal_name == available_animals[i].name)
+                {
+                    /* Sets the name and cost of the template variable to the
+                     * name and cost of the available animal that matches the
+                     * desired animal's name*/
+                    desired_animal.name = available_animals[i].name;
+                    desired_animal.cost = available_animals[i].cost;
+                }
+            }
+
+            // Checks if the user can afford the animal they want to purchase
+            if (make_purchase(desired_animal, budget))
+            {
+                // Stores the animal into the user's owned animals
+                owned_animals.insert(owned_animals.end(), desired_animal);
+            }
+        }
+
+        return;
+    }
+};
+
+int main()
+{
     // User's animals
-    vector<animal> owned_animals;
+    vector<Animal> owned_animals;
 
-    int budget = 4000;
-    string animal_name;
-    int animal_cost = 0;
+    // User's zoo
+    Zoo zoo("Vermont Zoo", owned_animals);
 
-    cout << "Your budget: " << budget << endl;
+    int budget = 4000000;
 
-    cout << "These are the animals you can buy: " << endl;
-    display_animals(available_animals);
+    for (int i = 0; i < 7; i++)
+    {
+        cout << endl << "Day " << i + 1 << endl;
+        cout << endl << "Your budget: " << budget << endl;
 
-    cout << endl << "Your animals: " << endl;
-    display_animals(owned_animals);
+        cout << "These are the animals you can buy: " << endl;
 
-    animals = purchase_animals(available_animals, owned_animals, animal_name, animal_cost, budget);
+        // Displays the available animals and the user's animals
+        display_animals(zoo.available_animals);
+
+        cout << endl << "Your animals: " << endl;
+
+        // Displays the available animals and the user's animals
+        display_animals(zoo.owned_animals);
+
+        // Allows the user to purchase animals
+        zoo.purchase_animals(budget);
+    }
 
     return 0;
 }
